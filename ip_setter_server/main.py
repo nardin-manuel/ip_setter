f50645fb44e01b5640d25c4b403b84247584ebf3
Manuel <Manuel@Manuel-Pc>
2016-08-08 00:18:58 +0200

    general changes

---
+++ ip_setter_server/main.py
@@ -0,0 +1,26 @@
+
+def read_file():
+    delimeter = " "
+    confirmed = "n"
+    line_list = {'mac':[], 'ip':[]}
+    
+    while (confirmed != "s"):
+        file_src = input("Inserisci la path del file: ")
+        print("il file selezionato e' il seguente:", file_src, "accettare?[s/N]")
+        confirmed = input()
+    file = open(file_src, "r")
+        
+    for line in file:
+        line = (line[:-1] if '\n' in line else line).split(delimeter, 1)  # tutta la linea tranne l'ultimo carattere(\n). Splitto sul delimeter. 1 solo delimimeter nella stringa
+        line_list['mac'].append(line[0])
+        line_list['ip'].append(line[1])
+    file.close()
+    return line_list
+    
+def find_ip_by_mac(line_list, mac):
+    return line_list['ip'][line_list['mac'].index(mac)]
+      
+  
+line_list = read_file()
+print(line_list)
+print(find_ip_by_mac(line_list, "cc:bb:aa:dd:ee:ff"))
