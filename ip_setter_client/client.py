917cf63501a8700f7ae96b3496586f611c69577b
Manuel <Manuel@Manuel-Pc>
2016-08-23 19:48:48 +0200

    spaces to tab general improvement to syntax

--- ip_setter_client/client.py
+++ ip_setter_client/client.py
@@ -3,50 +3,50 @@ import binascii
 from socket import *
 
 def receive(interface):
-     sock=socket(AF_PACKET, SOCK_RAW, htons(0x88b5))
-     sock.bind((interface,0))
-     print("Attendo pacchetto")
-     data=sock.recv(65536)
-     unpack(data)
-     sock.close()
+	sock = socket(AF_PACKET, SOCK_RAW, htons(0x88b5))
+	sock.bind((interface, 0))
+	print("Attendo pacchetto")
+	data = sock.recv(65536)
+	unpack(data)
+	sock.close()
 
 def unpack(packet):
-     mac_dst=decode_mac(packet[:6])
-     mac_src=decode_mac(packet[6:12])
-     print ("MAC DST: %s \nMAC SRC: %s"% ( mac_dst, mac_src))
-     print ("ETHERTYPE:", decode_ethertype(packet[12:14]))
-     print ("PAYLOAD:" , decode_payload(packet[14:]))
+	mac_dst = decode_mac(packet[:6])
+	mac_src = decode_mac(packet[6:12])
+	print ("MAC DST: %s \nMAC SRC: %s" % (mac_dst, mac_src))
+	print ("ETHERTYPE:", decode_ethertype(packet[12:14]))
+	print ("PAYLOAD:" , decode_payload(packet[14:]))
 
 def decode_mac(mac):
-     _=(binascii.hexlify(mac)).decode("ASCII")     
-     return ":".join([_[x:x+2] for x in range(0, len(_), 2)])
+	_ = (binascii.hexlify(mac)).decode("ASCII")	
+	return ":".join([_[x:x + 2] for x in range(0, len(_), 2)])
 
 def decode_ethertype(ethertype):
-     return binascii.hexlify(ethertype).decode("ASCII")
+	return binascii.hexlify(ethertype).decode("ASCII")
 
 def decode_payload(payload):
-     return payload.decode("ASCII")
+	return payload.decode("ASCII")
 
 def mac_to_hex(mac):
-    mac=mac[:-1] if mac[-1]=='\n' else mac
-    return binascii.unhexlify(mac.replace(":",""))
+    mac = mac[:-1] if mac[-1] == '\n' else mac
+    return binascii.unhexlify(mac.replace(":", ""))
 
 def request(mac_dst, interface, payload):
     # print(mac_src)
-     mac_source=mac_to_hex(open('/sys/class/net/'+interface+'/address').readline())
-#     mac_source=mac_to_hex(mac_source)
-     mac_dest = mac_to_hex(mac_dst)
-     ethertype=binascii.unhexlify("88B6")
-     payload=payload.encode("ASCII")
-     sock=socket(AF_PACKET, SOCK_RAW, htons(0x0003))
-     sock.bind((interface,0))
-#     print (mac_source)
-     sock.send(mac_dest+mac_source+ethertype+payload)
-     print("invio")
-     sock.close()
-i=0
+	mac_source = mac_to_hex(open('/sys/class/net/' + interface + '/address').readline())
+# 	mac_source=mac_to_hex(mac_source)
+	mac_dest = mac_to_hex(mac_dst)
+	ethertype = binascii.unhexlify("88B6")
+	payload = payload.encode("ASCII")
+	sock = socket(AF_PACKET, SOCK_RAW, htons(0x0003))
+	sock.bind((interface, 0))
+# 	print (mac_source)
+	sock.send(mac_dest + mac_source + ethertype + payload)
+	print("invio")
+	sock.close()
+i = 0
 while 1:
-     input("Premi invio per spedire un pacchetto")
-     request("ff:ff:ff:ff:ff:ff", "lo", "Voglio un ip"+str(i))
-     i+=1
-     receive("lo")
+	input("Premi invio per spedire un pacchetto")
+	request("ff:ff:ff:ff:ff:ff", "lo", "Voglio un ip" + str(i))
+	i += 1
+	receive("lo")
--- ip_setter_server/server.py
+++ ip_setter_server/server.py
@@ -3,92 +3,91 @@ import binascii
 import time
 from socket import *
 def read_file():
-    delimeter = " "
-    confirmed = "n"
-    line_list = {'mac':[], 'ip':[]}
-    
-    while (confirmed != "s"):
-        
-        file_src = input("Inserisci la path del file: ")
-        print("il file selezionato e' il seguente:", file_src, "accettare?[s/N]")
-        confirmed = input()
-    file = open(file_src, "r")
-        
-    for line in file:
-        line = (line[:-1] if '\n' in line else line).split(delimeter, 1)  # tutta la linea tranne l'ultimo carattere(\n). Splitto sul delimeter. 1 solo delimimeter nella stringa
-        print (line)
-        line_list['mac'].append(line[0])
-        line_list['ip'].append(line[1])
-    file.close()
-    return line_list
-    
+	delimeter = " "
+	confirmed = "n"
+	line_list = {'mac':[], 'ip':[]}
+	
+	while (confirmed != "s"):		
+		file_src = input("Inserisci la path del file: ")
+		print("il file selezionato e' il seguente:", file_src, "accettare?[s/N]")
+		confirmed = input()
+	file = open(file_src, "r")
+		
+	for line in file:
+		line = (line[:-1] if '\n' in line else line).split(delimeter, 1)  # tutta la linea tranne l'ultimo carattere(\n). Splitto sul delimeter. 1 solo delimimeter nella stringa
+		print (line)
+		line_list['mac'].append(line[0])
+		line_list['ip'].append(line[1])
+	file.close()
+	return line_list
+	
 def find_ip_by_mac(line_list, mac):
-    return line_list['ip'][line_list['mac'].index(mac)]
-      
+	return line_list['ip'][line_list['mac'].index(mac)]
+
 def mac_to_hex(mac):
-    mac=mac[:-1] if mac[-1]=='\n' else mac
-    return binascii.unhexlify(mac.replace(":",""))
+	mac = mac[:-1] if mac[-1] == '\n' else mac
+	return binascii.unhexlify(mac.replace(":", ""))
 
 def sendto(sock, mac_dst, interface, payload):
-     mac_source=mac_to_hex(open('/sys/class/net/'+interface+'/address').readline())
-     mac_dest = mac_to_hex(mac_dst)
-     ethertype=binascii.unhexlify("88B5")
-     _=("ip:192.168.1.1\n netmask:255.255.255.0\n gateway:192.168.1.254\n dns-nameservers:8.8.8.8-8.8.4.4")
-     payload=_.encode("ASCII")
-     sock.bind((interface,0))
-     print (mac_source)
-     sock.send(mac_dest+mac_source+ethertype+payload)
-     print("inviato")
-     
+	mac_source = mac_to_hex(open('/sys/class/net/' + interface + '/address').readline())
+	mac_dest = mac_to_hex(mac_dst)
+	ethertype = binascii.unhexlify("88B5")
+	_ = ("ip:192.168.1.1\n netmask:255.255.255.0\n gateway:192.168.1.254\n dns-nameservers:8.8.8.8-8.8.4.4")
+	payload = _.encode("ASCII")
+	sock.bind((interface, 0))
+	print (mac_source)
+	sock.send(mac_dest + mac_source + ethertype + payload)
+	print("inviato")
+	
 def open_socket_read():
-    return socket(AF_PACKET, SOCK_RAW, htons(0x88b6))
+	return socket(AF_PACKET, SOCK_RAW, htons(0x88b6))
 
 def open_socket_write():
-    return socket(AF_PACKET, SOCK_RAW, htons(0x88b5))
+	return socket(AF_PACKET, SOCK_RAW, htons(0x88b5))
 
 def receive(sock, interface):
-     sock.bind((interface,0))
-     data=sock.recv(65536)
-     return unpack(data)
+	sock.bind((interface, 0))
+	data = sock.recv(65536)
+	return unpack(data)
 
 def unpack(packet):
-     mac_dst=decode_mac(packet[:6])
-     mac_src=decode_mac(packet[6:12])
-     ethertype=decode_ethertype(packet[12:14]))
-     payload= decode_payload(packet[14:]))
-     print ("MAC DST: %s \nMAC SRC: %s" % (mac_dst, mac_src))
-     print ("PAYLOAD:", payload)
-     return mac_src, payload
+	mac_dst = decode_mac(packet[:6])
+	mac_src = decode_mac(packet[6:12])
+	ethertype = decode_ethertype(packet[12:14])
+	payload = decode_payload(packet[14:])
+	print ("MAC DST: %s \nMAC SRC: %s" % (mac_dst, mac_src))
+	print ("PAYLOAD:", payload)
+	return mac_src, payload
 
 def decode_mac(mac):
-     _=(binascii.hexlify(mac)).decode("ASCII")     
-     return ":".join([_[x:x+2] for x in range(0, len(_), 2)])
+	_ = (binascii.hexlify(mac)).decode("ASCII")	
+	return ":".join([_[x:x + 2] for x in range(0, len(_), 2)])
 
 def decode_ethertype(ethertype):
-     return binascii.hexlify(ethertype).decode("ASCII")
+	return binascii.hexlify(ethertype).decode("ASCII")
 
 def decode_payload(payload):
-     return payload.decode("ASCII")
+	return payload.decode("ASCII")
 
 
-#line_list = read_file()
-payload="l'indirizzo che avrai e': "
-sock_read=open_socket_read()
+# line_list = read_file()
+payload = "l'indirizzo che avrai e': "
+sock_read = open_socket_read()
 print("socket lettura aperto")
-open_write=0;
+open_write = 0;
 while 1:
-    print("Attendo client")
-    packet=receive(sock_read, "lo")
-    mac=packet[0]
-    payload_recv=packet[1]
-    if not open_write:
-        sock_write=open_socket_write()
-        print("socket scrittura aperto")
-        open_write=1
-    if open_write:
-        time.sleep(0.1)
-        #ip=find_ip_by_mac(line_list, mac) if payload_recv=="Voglio un ip"
-        #if ip==-1: 
-            #print("Errore, impossibile trovare l'indirizzo mac: %s nella tabella"%(mac))
-        #else:
-        sendto(sock_write, mac,"lo", payload)
+	print("Attendo client")
+	packet = receive(sock_read, "lo")
+	mac = packet[0]
+	payload_recv = packet[1]
+	if not open_write:
+		sock_write = open_socket_write()
+		print("socket scrittura aperto")
+		open_write = 1
+	if open_write:
+		time.sleep(0.1)
+		# ip=find_ip_by_mac(line_list, mac) if payload_recv=="Voglio un ip"
+		# if ip==-1: 
+		# print("Errore, impossibile trovare l'indirizzo mac: %s nella tabella"%(mac))
+		# else:
+		sendto(sock_write, mac, "lo", payload)
